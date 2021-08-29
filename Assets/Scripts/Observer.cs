1009c4c8db928bf83a1b63dd65d0992a097d5c4f
raihara3 <raihara3@gmail.com>
2021-08-29 17:44:34 +0900

    add project

---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] GameEnding gameEnding;
+
+    bool m_IsPlayerInRange;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_IsPlayerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_IsPlayerInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        if (m_IsPlayerInRange)
+        {
+            // AからBのベクトルは、B-Aである
+            // Vector3.up = (0, 1, 0)
+            Vector3 direction = player.position - transform.position + Vector3.up;
+
+            // raycasterの作成
+            Ray ray = new Ray(transform.position, direction);
+
+            RaycastHit raycastHit;
+            // outパラメータを使うことで、bool値だけでなくメソッドから情報を取得できる
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    gameEnding.CaughtPlayer();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent navMeshAgent;
+    // waypointを複数持てるよう配列にする
+    [SerializeField] Transform[] waypoints;
+    int m_CurrentWaypointIndex;
+
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    void Update()
+    {
+        // 目的地までの距離が、インスペクターで設定した到達とみなす範囲より小さいか
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            // indexがwaypoints.lengthに等しい場合、一番最初のwaypointに再設定
+            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+            // 次のwaypointに設定
+            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+        }
+    }
+}
